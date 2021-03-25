8b72e3e3a48c1cc0c57e996afc7d5dffd8234412
JasperKrol <jasper.paul.krol@gmail.com>
2021-03-25 19:12:30 +0100

    eerste oefeningen node en java

--- mathematical-operators.js
+++ mathematical-operators.js
@@ -4,27 +4,36 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 // 1. Declareer een variabele en sla daar de uitkomst van 4 plus 5 in op.                           // geeft 9 wanneer gelogd
 // ==========================================
 
+const outcome = 5 + 4;
+console.log(outcome)
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364 wanneer gelogd
 // ==========================================
 
+const multiply1 = 7 * 52;
+console.log(multiply1)
 
 // ==========================================
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49 wanneer gelogd
 // ==========================================
-
+const  mulitply2 = (4 + 3) * 7;
+console.log(mulitply2)
 
 // ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12 wanneer gelogd
 // ==========================================
-
+const divided = 36 / 6 *2;
+console.log(divided)
 
 // ==========================================
 // 5. Declareer twee variabelen en zet daarin respectievelijk de waardes 'zoet' en 'sappig'.
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig' wanneer gelogd
 // ==========================================
-
+const   taste = "zoet";
+const   taste1 = "sappig";
+const   totalTaste = taste+taste1;
+console.log(totalTaste)
 
 // ==========================================
 // 6. Declareer een variabele voor jouw voornaam en een variabele voor jouw achternaam en zet de waardes erin.
@@ -32,9 +41,18 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters' wanneer gelogd
 // ==========================================
 
+const firstName = "jasper";
+const secondName = "krol";
+const fullName = firstName + " " + secondName;
+console.log(fullName)
 
 // ==========================================
 // 7. Declareer een variabele voor jouw woonplaats en een variabele voor provincie en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg wanneer gelogd
 // ==========================================
+
+const city = "Utrecht";
+const province = "Utrecht";
+const livingArea = city + ", " + province;
+console.log(livingArea)
--- variables.js
+++ variables.js
@@ -4,41 +4,55 @@
 
 const start = "Let's go!";
 
+
 // ==========================================
 // 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
+
+const name = "jasper";
+
 
 
 // ==========================================
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 
+const eyeColor = "Green";
+
 
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
+
+let age = 30;
 
 
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let currentJob = "Banker";
 
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
+
+const shoeSize = 46;
 
 
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let hasPartner = true;
 
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let numberOfChildren = 0;
 
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
+console.log(start, name, eyeColor, age, currentJob, shoeSize, hasPartner, numberOfChildren)
